38075019e8dd13085732d4528203412da79ff3f2
Heloisa Felizardo <heloisa.felizardo@yahoo.com>
2023-05-15 12:18:14 -0300

    style: styles.css, feat: favicon, modified: scripts

--- js/scripts.js
+++ js/scripts.js
@@ -17,39 +17,40 @@ const keys = document.querySelectorAll('.key');
 keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
 ////////////////////////////////////////////////////////////////////////////////////////
 
+const divs = document.querySelectorAll('[data-key]');
+
+// Adicionar o evento 'touchstart' a cada div
+divs.forEach((div) => {
+	div.addEventListener('touchstart', function (event) {
+		// Prevenir o comportamento padrão do evento
+		event.preventDefault();
+
+		// Obter o valor do atributo 'data-key'
+		const dataKey = this.getAttribute('data-key');
+
+		// Chamar a função desejada com base no valor do 'data-key'
+		playSoundTouch(dataKey);
+	});
+});
+
+// Função chamada quando ocorre o evento 'touchstart'
+function playSoundTouch(dataKey) {
+	console.log('Div clicada com data-key:', dataKey);
+	// Realize qualquer ação desejada com base no valor do 'data-key'
+	const audio = document.querySelector(`audio[data-key='${dataKey}']`);
+	const key = document.querySelector(`.key[data-key='${dataKey}']`);
+	audio.currentTime = 0; //rewind to start
+	audio.play();
+	key.classList.add('playing');
+}
+
 if (
 	/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
 		navigator.userAgent
 	)
 ) {
 	// Selecionar todas as divs com o atributo 'data-key'
-	const divs = document.querySelectorAll('[data-key]');
 
-	// Adicionar o evento 'touchstart' a cada div
-	divs.forEach(function (div) {
-		div.addEventListener('touchstart', function (event) {
-			// Prevenir o comportamento padrão do evento
-			event.preventDefault();
-
-			// Obter o valor do atributo 'data-key'
-			var dataKey = this.getAttribute('data-key');
-
-			// Chamar a função desejada com base no valor do 'data-key'
-			minhaFuncao(dataKey);
-		});
-	});
-
-	// Função chamada quando ocorre o evento 'touchstart'
-	function minhaFuncao(dataKey) {
-		console.log('Div clicada com data-key:', dataKey);
-		// Realize qualquer ação desejada com base no valor do 'data-key'
-		const audio = document.querySelector(`audio[data-key='${dataKey}']`);
-		const key = document.querySelector(`.key[data-key='${dataKey}']`);
-		console.log(audio, key);
-		audio.currentTime = 0; //rewind to start
-		audio.play();
-		key.classList.add('playing');
-	}
 	console.log('mobile');
 } else {
 	window.addEventListener('keydown', playSound);
